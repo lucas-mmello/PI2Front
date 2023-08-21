b129274482182832a6f07dd007556dcc77685bc0
lucas mello <lucasmello03@hotmail.com>
2023-08-21 20:12:47 -0300

    fiz alguns testes com o carrosel na home e com o footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
             href="/"
             className="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1"
           >
-            <i className="bi bi-brush-fill icon"></i>
+            <i className="bi bi-brush-fill "></i>
           </a>
           <span className="mb-3 mb-md-0 text-body">
             &copy; 2023 Company, Inc
@@ -17,17 +17,17 @@ export default function Footer() {
 
         <ul className="nav col-md-4 justify-content-end px-4 list-unstyled d-flex">
           <li className="ms-3">
-            <a className="text-body-secondary" href="#">
+            <a className="text-ligth" href="#">
               <i className="bi bi-twitter"></i>
             </a>
           </li>
           <li className="ms-3">
-            <a className="text-body-secondary" href="#">
+            <a className="text-danger" href="#">
               <i className="bi bi-instagram"></i>
             </a>
           </li>
           <li className="ms-3">
-            <a className="text-body-secondary" href="#">
+            <a className="text-light" href="#">
               <i className="bi bi-facebook"></i>
             </a>
           </li>
