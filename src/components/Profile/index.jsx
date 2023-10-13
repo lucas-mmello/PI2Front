6d15fee06a8fbab6b29f9cefaa3f5455b3296516
lucas mello <lucasmello03@hotmail.com>
2023-10-13 15:54:30 -0300

    arrumei o profile

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,27 +1,31 @@
+/* Profile.js */
+
 import styles from "./styles.module.scss";
 import { Link } from "react-router-dom";
 
 export default function Profile({ studioInfo, ownProfile }) {
   return (
-    <div className={styles.header}>
-      <div className={styles.studioInfo}>
-        <h2>{studioInfo.name}</h2>
-        <p>{studioInfo.address}</p>
-        <p>
-          {studioInfo.city}, {studioInfo.state}
-        </p>
-        <p>CNPJ: {studioInfo.cnpj}</p>
-      </div>
-      <div className={styles.contactInfo}>
-        <p>Telefone: {studioInfo.phone}</p>
-        <p>Celular: {studioInfo.cellphone}</p>
-        {ownProfile && (
-          <button className="btn btn-info">
-            <Link to="/private/estudioPrivate/styles" className={styles.link}>
-              <i className="bi bi-brush pe-2"></i> Ver Estilos
-            </Link>
-          </button>
-        )}
+    <div className={styles.container}>
+      <h2 className="mb-4">{studioInfo.name}</h2>
+      <div className={styles.grid}>
+        <div className={styles.studioInfo}>
+          <p>{studioInfo.address}</p>
+          <p>
+            {studioInfo.city}, {studioInfo.state}
+          </p>
+          <p>CNPJ: {studioInfo.cnpj}</p>
+        </div>
+        <div className={styles.contactInfo}>
+          <p>Telefone: {studioInfo.phone}</p>
+          <p>Celular: {studioInfo.cellphone}</p>
+          {ownProfile && (
+            <button className="btn btn-info">
+              <Link to="/private/estudioPrivate/styles" className={styles.link}>
+                <i className="bi bi-brush pe-2"></i> Ver Estilos
+              </Link>
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
