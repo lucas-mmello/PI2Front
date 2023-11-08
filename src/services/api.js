de469422a8d052552fef63e5213b0e55953055b7
lucas mello <lucasmello03@hotmail.com>
2023-11-08 10:40:37 -0300

    adicionei o endereço da api publicada

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-  baseURL: "https://inksearchpi2.azurewebsites.net",
+  baseURL: "https://inksearchprojetoi2.azurewebsites.net",
 });
 
 export default Api;
