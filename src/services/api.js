2037c274eb4f7d74a6e541bf9f0fad70c763d99b
lucas mello <lucasmello03@hotmail.com>
2023-11-24 13:32:54 -0300

    resolvi o  erro

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-  baseURL: "https://localhost:44325",
+  baseURL: "https://inksearchprojetoi2.azurewebsites.net",
 });
 
 export default Api;
