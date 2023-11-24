03ab5fd6e2f37d681388a42a45c39167375e90ec
lucas mello <lucasmello03@hotmail.com>
2023-11-24 13:22:01 -0300

    corrigi um erro

--- src/components/Auth/Register/EstudioRegister/index.jsx
+++ src/components/Auth/Register/EstudioRegister/index.jsx
@@ -42,7 +42,7 @@ export default function EstudioRegister(props) {
     return str
       .normalize("NFD")
       .replace(/[\u0300-\u036f]/g, "")
-      .replace(/[^a-zA-Z0-9]/g, "");
+      .replace(/[^a-zA-Z0-9\s]/g, "");
   };
 
   const handleFormSubmit = async (event) => {
