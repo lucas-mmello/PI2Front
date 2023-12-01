8488c11842b570aa1e0069b10d3519901b075a6d
lucas mello <lucasmello03@hotmail.com>
2023-12-01 01:22:09 -0300

    tentando melhorar o cardpost

--- src/components/Card/Post/index.jsx
+++ src/components/Card/Post/index.jsx
@@ -5,40 +5,36 @@ export default function Post(props) {
   const { image, description, onEdit, onDelete, idModalEd, idModalDel } = props;
 
   return (
-    <div className={`card ${styles.cardPost} h-100 d-flex flex-column`}>
-      <img
-        src={image}
-        className="card-img-top flex-grow-1"
-        alt="Imagem do Post"
-      />
-      <div className={`card-body d-flex flex-column ${styles.postBody}`}>
+    <div className={`card ${styles.cardPost} h-100`}>
+      <div className={styles.imageContainer}>
+        <img src={image} className={`card-img-top`} alt="Imagem do Post" />
+      </div>
+      <div className={`card-body ${styles.actions}`}>
         <p className="card-text">{description}</p>
-        <div className="mt-auto">
-          {onEdit && (
-            <button
-              data-bs-toggle="modal"
-              data-bs-target={idModalEd}
-              onClick={onEdit}
-              className={`card-link ${styles.button}`}
-            >
-              <span>
-                <i className={`bi bi-pencil ${styles.icon}`}></i>
-              </span>
-            </button>
-          )}
-          {onDelete && (
-            <button
-              data-bs-toggle="modal"
-              data-bs-target={idModalDel}
-              onClick={onDelete}
-              className={`card-link ${styles.button}`}
-            >
-              <span>
-                <i className={`bi bi-trash3 ${styles.icon}`}></i>
-              </span>
-            </button>
-          )}
-        </div>
+        {onEdit && (
+          <button
+            data-bs-toggle="modal"
+            data-bs-target={idModalEd}
+            onClick={onEdit}
+            className={`card-link ${styles.button}`}
+          >
+            <span>
+              <i className={`bi bi-pencil ${styles.icon}`}></i>
+            </span>
+          </button>
+        )}
+        {onDelete && (
+          <button
+            data-bs-toggle="modal"
+            data-bs-target={idModalDel}
+            onClick={onDelete}
+            className={`card-link ${styles.button}`}
+          >
+            <span>
+              <i className={`bi bi-trash3 ${styles.icon}`}></i>
+            </span>
+          </button>
+        )}
       </div>
     </div>
   );
