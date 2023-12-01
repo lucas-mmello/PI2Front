2703a9b6462078ab15e49eed3396911d93c36227
lucas mello <lucasmello03@hotmail.com>
2023-12-01 00:35:28 -0300

    mudando a paleta de cores

--- src/components/Card/Post/index.jsx
+++ src/components/Card/Post/index.jsx
@@ -5,8 +5,12 @@ export default function Post(props) {
   const { image, description, onEdit, onDelete, idModalEd, idModalDel } = props;
 
   return (
-    <div className={`card ${styles.cardPost} h-100`}>
-      <img src={image} className="card-img-top" alt="Imagem do Post" />
+    <div className={`card ${styles.cardPost} h-100 d-flex flex-column`}>
+      <img
+        src={image}
+        className="card-img-top flex-grow-1"
+        alt="Imagem do Post"
+      />
       <div className="card-body">
         <p className="card-text">{description}</p>
       </div>
